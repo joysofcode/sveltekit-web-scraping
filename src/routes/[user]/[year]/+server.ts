d7dd9ae20421051101cb2f1fa58e505e34b2496c
bshore <shore7@gmail.com>
2023-12-06 00:13:03 -0600

    fix: github contributions table parsing

--- src/routes/[user]/[year]/+server.ts
+++ src/routes/[user]/[year]/+server.ts
@@ -3,56 +3,68 @@ import { parseHTML } from 'linkedom'
 import type { RouteParams } from './$types.js'
 
 export async function GET({ params, setHeaders }) {
-	setHeaders({
-		'Acces-Control-Allow-Origin': '*',
-		'Cache-Control': `public, s-maxage=${60 * 60 * 24 * 365}`,
-	})
+  setHeaders({
+    'Acces-Control-Allow-Origin': '*',
+    'Cache-Control': `public, s-maxage=${60 * 60 * 24 * 365}`,
+  })
 
-	const html = await getContributions(params)
-	return json(parseContributions(html))
+  const html = await getContributions(params)
+  return json(parseContributions(html))
 }
 
 async function getContributions({ user, year }: RouteParams) {
-	const api = `https://github.com/users/${user}/contributions?from=${year}-12-01&to=${year}-12-31`
-	const response = await fetch(api)
+  const api = `https://github.com/users/${user}/contributions?from=${year}-12-01&to=${year}-12-31`
+  const response = await fetch(api)
 
-	if (!response.ok) {
-		throw new Error(`Failed to fetch: ${response.status}`)
-	}
+  if (!response.ok) {
+    throw new Error(`Failed to fetch: ${response.status}`)
+  }
 
-	return await response.text()
+  return await response.text()
 }
 
 function parseContributions(html: string) {
-	const { document } = parseHTML(html)
+  const { document } = parseHTML(html)
 
-	const rows = document.querySelectorAll<HTMLTableRowElement>('tbody > tr')
+  const days = document.querySelectorAll<Element>('tool-tip')
 
-	const contributions = []
+  const contributions: any[][] = [
+    [], // Sundays
+    [], // Mondays
+    [], // Tuesdays
+    [], // Wednesdays
+    [], // Thursdays
+    [], // Fridays
+    [], // Saturdays
+  ]
 
-	for (const row of rows) {
-		const days = row.querySelectorAll<HTMLTableCellElement>('td:not(.ContributionCalendar-label)')
+  for (const [i, day] of days.entries()) {
+    const data = day.innerHTML.split(' ')
 
-		const currentRow = []
+    const forDayRaw = day.getAttribute('for')
 
-		for (const day of days) {
-			const data = day.innerText.split(' ')
+    if (!forDayRaw) {
+      continue
+    }
+    const forDay = forDayRaw.replace('contribution-day-component-', '')
+    const [weekDayStr, weekStr] = forDay.split('-')
+    const weekday = +weekDayStr
+    const week = +weekStr
 
-			if (data.length > 1) {
-				const contribution = {
-					count: data[0] === 'No' ? 0 : +data[0],
-					month: data[3],
-					day: data[4].replace('.', ''),
-					level: +day.dataset.level!,
-				}
-				currentRow.push(contribution)
-			} else {
-				currentRow.push(null)
-			}
-		}
-
-		contributions.push(currentRow)
-	}
-
-	return contributions
+    if (data.length > 1) {
+      const td = document.getElementById(forDayRaw)
+      if (!td) {
+        continue
+      }
+      const level = td.dataset.level || '0'
+      const contribution = {
+        count: data[0] === 'No' ? 0 : +data[0],
+        month: data[3],
+        day: +data[4].replace(/(st|nd|rd|th)/, ''),
+        level: +level,
+      }
+      contributions[weekday][week] = contribution
+    }
+  }
+  return contributions
 }
